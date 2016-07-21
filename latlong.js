2abeac625ac27448e55fcc30f94b06453aad1d67
Christopher King <chrisking1@gmail.com>
2016-07-21 15:45:52 -0400

    script for posting address and getting latitude and longitude

--- latlong.js
+++ latlong.js
@@ -1,7 +1,6 @@
-$( document ).ready(function() {
-
-  let myAddress = 'France';
-
+let myAddress = 'France';
+let latitude;
+let longitude;
 
   $.ajax({
     url: 'https://maps.googleapis.com/maps/api/geocode/json',
@@ -9,8 +8,8 @@ $( document ).ready(function() {
     data: {'address' : myAddress, 'key' : 'AIzaSyA4Bt3-KoRWpmLj2gCrN04Fed3mcZRYfKQ'},
     success: function(data){
       console.log('Latitude is: ' + data.results[0].geometry.location.lat + ' and longitude is: ' + data.results[0].geometry.location.lng);
-    }
-  });
+      latitude = data.results[0].geometry.location.lat;
+      longitude = data.results[0].geometry.location.lng;
+      }
+    });
 
-
-});
